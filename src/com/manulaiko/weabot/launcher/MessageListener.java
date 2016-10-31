eed5f2e6e43caa325baa4b900392224747590d43
Manulaiko <manulaiko@gmail.com>
2016-10-31 09:22:01 +0100

    Created MessageListener class

---
+++ src/com/manulaiko/weabot/launcher/MessageListener.java
@@ -0,0 +1,54 @@
+package com.manulaiko.weabot.launcher;
+
+import java.util.ArrayList;
+
+import com.manulaiko.tabitha.Console;
+import com.manulaiko.tabitha.utils.ICommand;
+import net.dv8tion.jda.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.hooks.ListenerAdapter;
+
+/**
+ * Message listener class.
+ *
+ * Listens for messages and process them.
+ *
+ * @author Manulaiko <manulaiko@gmail.com>
+ */
+public class MessageListener extends ListenerAdapter
+{
+    /**
+     * Available commands array.
+     *
+     * @var Available commands.
+     */
+    public ArrayList<ICommand> commands = new ArrayList();
+
+    /**
+     * Constructor.
+     */
+    public MessageListener()
+    {
+        super();
+    }
+
+    /**
+     * Listens and process messages.
+     *
+     * @param event Fired event.
+     */
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        if(Main.configuration.getBoolean("bot.printMessages")) {
+            Console.println("["+ event.getTextChannel().getName() +"] ("+ event.getAuthorName() +"): "+ event.getMessage());
+        }
+
+        String[] command = event.getMessage().getContent().split(" ");
+
+        this.commands.forEach((c)->{
+            if(c.canExecute(command[0])) {
+                c.execute(command);
+            }
+        });
+    }
+}
