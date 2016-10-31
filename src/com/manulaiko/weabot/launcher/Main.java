063fba355877a5f610b06cdf6e045c42685669c0
Manulaiko <manulaiko@gmail.com>
2016-10-31 08:34:48 +0100

    Created Main class

---
+++ src/com/manulaiko/weabot/launcher/Main.java
@@ -0,0 +1,21 @@
+package com.manulaiko.weabot.launcher;
+
+/**
+ * Weabot main class
+ *
+ * Contains main method and bootstraps the application.
+ *
+ * @author Manulaiko <manulaiko@gmail.com>
+ */
+public class Main
+{
+    /**
+     * Main method.
+     *
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args)
+    {
+
+    }
+}
