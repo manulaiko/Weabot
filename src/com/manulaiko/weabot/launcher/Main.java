7c8caf675cd125fdcdd109092d958bb580f13f1a
Manulaiko <manulaiko@gmail.com>
2016-12-01 18:15:55 +0100

    Versioning

--- src/com/manulaiko/weabot/launcher/Main.java
+++ src/com/manulaiko/weabot/launcher/Main.java
@@ -27,7 +27,7 @@ public class Main
      *
      * @var Version.
      */
-    private static final String version = "0.0.0";
+    private static final String version = "1.0.0";
 
     /**
      * Configuration object.
